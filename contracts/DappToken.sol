549ea78cc57ce87c624cea43d462a3cf3b265c2b
Ninh Le <levanninh0101@gmail.com>
2018-08-18 16:19:28 +0700

    name, symbol and standard

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,15 +1,17 @@
 pragma solidity ^0.4.2;
 
 contract DappToken { 
-    // Contructor
-
-     // Set the total number of tokens
-
-     // Read the totial number of tokens
+   
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0";
     uint256 public totalSupply; 
     
-    function DappToken() public { 
-        totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
 
+    constructor(uint256 _initialSupply) public { 
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        // allocate the initial supply
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -2,12 +2,33 @@ var DappToken = artifacts.require("./DappToken.sol");
 
 contract('DappToken', function(accounts) {
 
-  it('sets the total supply upon deployment', function() {
-    return DappToken.deployed().then(function(instance) {
-      tokenInstance = instance;
-      return tokenInstance.totalSupply();
-    }).then(function(totalSupply) {
-      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
-    });
+  it('initializes the contract with the correct values', async ()=> {
+    let tokenInstance = await DappToken.deployed();
+    let name = await tokenInstance.name();
+    assert.equal(name, 'DApp Token', 'has the correct name');
+    let symbol = await tokenInstance.symbol();
+    assert.equal(symbol, 'DAPP', 'has the correct symbol');
+    let standard = await tokenInstance.standard();
+    assert.equal(standard, 'DApp Token v1.0', 'has the correct standard');
+  })
+
+
+  it('sets the total supply upon deployment', async () => {
+    let tokenInstance = await DappToken.deployed();
+    let totalSupply = await tokenInstance.totalSupply();
+    assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+    let  adminBalance = await tokenInstance.balanceOf(accounts[0]);
+    assert.equal(adminBalance.toNumber(), 1000000 , 'it allocates the initial supply to the admin account');
+    
+    // return DappToken.deployed().then(function(instance) {
+    //   tokenInstance = instance;
+    //   return tokenInstance.totalSupply();
+    // }).then(function(totalSupply) {
+    //   assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+    //   return tokenInstance.balanceOf(accounts[0]);
+    // }).then( function(adminBalance) { 
+    //   assert.equal(adminBalance.toNumber(), 1000000 , 'it allocates the initial supply to the admin account');
+    // } )
   });
 })
+ 
